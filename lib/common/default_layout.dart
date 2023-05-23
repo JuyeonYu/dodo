b393c201f61d37dfdf16e5788f567f09a3b6abe7
juyeon <remake867@gmail.com>
2023-05-23 20:10:58 +0900

    init

---
+++ lib/common/default_layout.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'const/colors.dart';
+
+class DefaultLayout extends StatelessWidget {
+  final Widget child;
+  final Color? backgroundColor;
+  final String? title;
+  final Widget? bottomNavigationBar;
+  final FloatingActionButton? floatingActionButton;
+  final List<Widget>? actions;
+  final Widget? leading;
+
+  const DefaultLayout({
+    required this.child,
+    this.backgroundColor,
+    this.title,
+    this.bottomNavigationBar,
+    this.floatingActionButton,
+    this.actions,
+    this.leading,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColor ?? Colors.white,
+      appBar: renderAppBar(),
+      bottomNavigationBar: bottomNavigationBar,
+      body: SafeArea(child: child),
+      floatingActionButton: floatingActionButton,
+    );
+  }
+  AppBar? renderAppBar() {
+    if (title == null) {
+      return null;
+    } else {
+      return AppBar(
+        leading: leading,
+        actions: actions,
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Text(
+          title!,
+          style: TextStyle(
+            fontSize: 16.0,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        foregroundColor: Colors.black,
+      );
+    }
+  }
+}
