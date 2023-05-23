4daefaa3e3a67550221da6a304b0621ae9e6b005
juyeon <remake867@gmail.com>
2023-05-24 02:05:04 +0900

    feat: tile에 swipe 제스처 넣기

--- lib/todo/todo_screen.dart
+++ lib/todo/todo_screen.dart
@@ -18,21 +18,6 @@ class TodoScreen extends StatefulWidget {
 }
 
 class _TodoScreenState extends State<TodoScreen> {
-  final List<Section> sections = [
-    Section(
-      title: 'Section 1',
-      items: ['Item 1', 'Item 2', 'Item 3'],
-    ),
-    Section(
-      title: 'Section 2',
-      items: ['Item 4', 'Item 5'],
-    ),
-    Section(
-      title: 'Section 3',
-      items: ['Item 6', 'Item 7', 'Item 8'],
-    ),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<QuerySnapshot>(
@@ -47,146 +32,114 @@ class _TodoScreenState extends State<TodoScreen> {
             child: CircularProgressIndicator(),
           );
         }
-        List<Todo> todoList = [];
 
-        snapshot.data!.docs.forEach((doc) {
+        List<DocumentSnapshot> todoDocs = snapshot.data!.docs;
+        List<DocumentSnapshot> completedTodos = [];
+        List<DocumentSnapshot> pendingTodos = [];
+        for (var doc in todoDocs) {
           Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
-          Todo todo = Todo.fromJson(data);
-          todo.id = doc.id;
-          todoList.add(todo);
-        });
+          bool isDone = data['isDone'];
+          if (isDone) {
+            completedTodos.add(doc);
+          } else {
+            pendingTodos.add(doc);
+          }
+        }
 
-        return ListView.builder(
-          itemCount: todoList.length,
-          itemBuilder: (context, index) {
-            Todo todo = todoList[index];
-            return ListTile(
-              selected: todo.isDone,
-              selectedColor: BODY_TEXT_COLOR,
-              selectedTileColor: Colors.black26,
-              onTap: () {
-
-                setState(() {
-
-                  // todo.isDone = !todo.isDone;
-                });
-              },
-              leading: Container(color: hexToColor(todo.colorCode), child: SizedBox(width: 10, height: 500,)),
-
-              title: Text(todo.title),
-              trailing: Checkbox(
-                value: todo.isDone,
-                onChanged: (value) {
-                  firestore.collection('todo').doc(todo.id).update({
-                    'isDone': !todo.isDone
-                  });
-                },
-              ),
-            );
-          },
-        );
-      },
-    );
-    return ListView.builder(
-      itemCount: sections.length,
-      itemBuilder: (context, sectionIndex) {
-        final section = sections[sectionIndex];
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        return ListView(
           children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                section.title,
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16,
-                ),
-              ),
-            ),
-            ListView.builder(
-              shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),
-              itemCount: section.items.length,
-              itemBuilder: (context, itemIndex) {
-                final item = section.items[itemIndex];
-                return ListTile(
-                  title: Text(item),
-                );
-              },
-            ),
+            _buildSection('Pending', pendingTodos),
+            _buildSection('Completed', completedTodos),
           ],
         );
       },
     );
   }
-}
-
-
-class SectionedListViewExample extends StatelessWidget {
-  final List<Section> sections = [
-    Section(
-      title: 'Section 1',
-      items: ['Item 1', 'Item 2', 'Item 3'],
-    ),
-    Section(
-      title: 'Section 2',
-      items: ['Item 4', 'Item 5'],
-    ),
-    Section(
-      title: 'Section 3',
-      items: ['Item 6', 'Item 7', 'Item 8'],
-    ),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Sectioned ListView Example'),
-      ),
-      body: ListView.builder(
-        itemCount: sections.length,
-        itemBuilder: (context, sectionIndex) {
-          final section = sections[sectionIndex];
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  section.title,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16,
+  Widget _buildSection(String sectionName, List<DocumentSnapshot> todos) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            sectionName,
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        ListView.builder(
+          shrinkWrap: true,
+          physics: ClampingScrollPhysics(),
+          itemCount: todos.length,
+          itemBuilder: (context, index) {
+            DocumentSnapshot doc = todos[index];
+            Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+            Todo todo = Todo.fromJson(data);
+            todo.id = doc.id;
+            return Dismissible(
+              background: Container(
+                color: Colors.red,
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: Text('to you'),
                   ),
                 ),
               ),
-              ListView.builder(
-                shrinkWrap: true,
-                physics: NeverScrollableScrollPhysics(),
-                itemCount: section.items.length,
-                itemBuilder: (context, itemIndex) {
-                  final item = section.items[itemIndex];
-                  return ListTile(
-                    title: Text(item),
-                  );
-                },
+              secondaryBackground: Container(
+                color: Colors.green,
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: Icon(
+                      Icons.check,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
               ),
-            ],
-          );
-        },
-      ),
+              key: Key(todo.id ?? ""),
+              child: ListTile(
+                selected: todo.isDone,
+                selectedColor: BODY_TEXT_COLOR,
+                selectedTileColor: Colors.white10,
+                onTap: () {
+
+                  // setState(() {
+                  //
+                  //   // todo.isDone = !todo.isDone;
+                  // });
+                },
+                leading: Container(color: hexToColor(todo.colorCode), child: SizedBox(width: 10, height: 500,)),
+
+                title: Text(todo.title),
+                trailing: Checkbox(
+                  value: todo.isDone,
+                  onChanged: (value) {
+                    firestore.collection('todo').doc(todo.id).update({
+                      'isDone': !todo.isDone
+                    });
+                  },
+                ),
+              ),
+              onDismissed: (direction) {
+                if (direction == DismissDirection.startToEnd) {
+                  // 좌측에서 우측으로 스와이프됨 (삭제 액션)
+                  // _deleteTodoItem(todo);
+                } else if (direction == DismissDirection.endToStart) {
+                  // 우측에서 좌측으로 스와이프됨 (완료 액션)
+                  // _completeTodoItem(todo);
+                }
+              },
+
+            );
+          },
+        ),
+      ],
     );
   }
-}
-
-class Section {
-  final String title;
-  final List<String> items;
-
-  Section({
-    required this.title,
-    required this.items,
-  });
 }
