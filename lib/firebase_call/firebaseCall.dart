2d17efbef5f4e0062e73006e17f9560e5d3c35d4
juyeon <remake867@gmail.com>
2023-06-09 23:09:22 +0900

    feat: 게스트의 푸시 토큰은 수집하지 않음

--- lib/firebase_call/firebaseCall.dart
+++ lib/firebase_call/firebaseCall.dart
@@ -1,13 +1,12 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import '../common/const/data.dart';
 
 Future<void> insertUser() async {
-  var response = (await firestore
-      .collection('user')
-      .doc(getUserId())
-      .get()).data();
+  var response =
+      (await firestore.collection('user').doc(getUserId()).get()).data();
   var token = await FirebaseMessaging.instance.getToken();
-  if (response == null) {
+  if (response == null && FirebaseAuth.instance.currentUser?.email != null) {
     await firestore
         .collection('user')
         .doc(getUserId())
