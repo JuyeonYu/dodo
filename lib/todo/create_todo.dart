0fdf258f748d09c106afd01fdb823cb99c311f9a
juyeon <remake867@gmail.com>
2023-06-19 00:58:27 +0900

    chore

--- lib/todo/create_todo.dart
+++ lib/todo/create_todo.dart
@@ -1,4 +1,3 @@
-import 'package:dodo/common/util/FCMController.dart';
 import 'package:dodo/user/model/partner_provider.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
