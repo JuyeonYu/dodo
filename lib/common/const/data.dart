80692c1b43d72e9133682ab1b9fb6aaf38a4613e
juyeon <remake867@gmail.com>
2023-11-06 20:39:31 +0900

    feat: 할일에 요일 추가

--- lib/common/const/data.dart
+++ lib/common/const/data.dart
@@ -6,6 +6,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:intl/intl.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 final storage = FlutterSecureStorage();
 final firestore = FirebaseFirestore.instance;
@@ -20,4 +21,4 @@ const androidBannerId = 'ca-app-pub-7604048409167711/8529511847';
 const iOSBannerId = 'ca-app-pub-7604048409167711/5871558919';
 
 List<Color> labelColors = [Colors.grey, PRIMARY_COLOR, Colors.redAccent];
-DateFormat expirationDateFormat = DateFormat('yyyy. MM. dd');
+DateFormat expirationDateFormat = DateFormat('yyyy. MM. dd(E)');
